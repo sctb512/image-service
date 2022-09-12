c7069dd6ec41e60dc8115e3190a2d49cfba02b91
Changwei Ge <gechangwei@bytedance.com>
2022-09-12 11:01:07 +0800

    upgrade: add mock mem states storage
    Signed-off-by: Changwei Ge <gechangwei@bytedance.com>

--- upgrade/src/states_storage.rs
+++ upgrade/src/states_storage.rs
@@ -33,3 +33,52 @@ impl StatesStorage for SocketStatesStorage {
         Ok((len, fds_cnt))
     }
 }
+
+#[cfg(test)]
+pub mod mem_storage {
+    use std::io::Result;
+    use std::ops::Deref;
+    use std::os::unix::io::RawFd;
+    use std::sync::Mutex;
+
+    use super::StatesStorage;
+    pub struct MemoryStatesStorage {
+        states: Mutex<(Vec<u8>, usize)>,
+        cap: usize,
+    }
+
+    impl MemoryStatesStorage {
+        pub fn new(cap: usize) -> Self {
+            Self {
+                states: Mutex::new((Vec::new(), 0)),
+                cap,
+            }
+        }
+    }
+
+    impl StatesStorage for MemoryStatesStorage {
+        fn write(&self, buf: &[u8], _fds: &[RawFd]) -> Result<()> {
+            let mut guard = self.states.lock().unwrap();
+
+            if guard.1 + buf.len() > self.cap {
+                return Err(std::io::Error::new(std::io::ErrorKind::OutOfMemory, ""));
+            }
+
+            guard.0.extend_from_slice(buf);
+            guard.1 += buf.len();
+
+            Ok(())
+        }
+
+        fn read(&self, buf: &mut [u8], _fds: &mut [RawFd]) -> Result<(usize, usize)> {
+            let guard = self.states.lock().unwrap();
+            let pos = guard.deref().1;
+
+            for (i, byte) in guard.deref().0.iter().take(pos).enumerate() {
+                buf[i] = *byte;
+            }
+
+            Ok((pos, 0))
+        }
+    }
+}
