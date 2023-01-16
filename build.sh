63c4605a86d29e15d3756fb8fddcc236353d29d4
Changwei Ge <gechangwei@bytedance.com>
2023-01-16 12:59:18 +0800

    build static nydus binaries
    Signed-off-by: Changwei Ge <gechangwei@bytedance.com>

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 
 set -e
 
-make release
+BINARIES_DIR="target/x86_64-unknown-linux-musl/release"
+
+rustup target add x86_64-unknown-linux-musl
+$(which sudo) apt install -y musl-dev
+
+make static-release
 
 mkdir -p output
 
-install -D -m 755 target/release/nydusd output
-install -D -m 755 target/release/nydus-image output
-install -D -m 755 target/release/nydusctl output
+install -D -m 755 ${BINARIES_DIR}/nydusd output
+install -D -m 755 ${BINARIES_DIR}/nydus-image output
+install -D -m 755 ${BINARIES_DIR}/nydusctl output
